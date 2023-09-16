d0c2c827bffa867411678e33396daaf17f45bf86
Emily Gulhak <118759000+Emilygulhak@users.noreply.github.com>
2023-09-15 22:57:16 -0300

    Add files via upload

---
+++ SQL_INSERT.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios (id, nome, email, data_nascimento, endereco) VALUES (1, 'Emily Gulhak', 'gulhak.contatos@gmail.com', '10/03/2001', 'Rua dos Fernandes');
+
+INSERT INTO viagens_destinos (id, nome, descricao) VALUES (1, 'Curitiba', 'Bastante frio');
+
+INSERT INTO viagens_reservas (id, id_usuario, id_destino, status, data) VALUES (1, 1, 1, 'Pendente', '11/11/2023');
